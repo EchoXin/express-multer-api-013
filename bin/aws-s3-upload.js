64506c746fcdab4b2e1f9cb60168931144e4e059
EchoXin <xin.yi@husky.neu.edu>
2016-09-01 10:53:26 -0400

    create s3upload module in lib/aws-s3-upload.js
    use s3upload module in bin/aws-s3-upload.js

--- bin/aws-s3-upload.js
+++ bin/aws-s3-upload.js
@@ -2,42 +2,11 @@
 
 require('dotenv').load();
 
-const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
-const fs = require('fs');
-const mime = require('mime');
-const path = require('path');
+const s3Upload = require('../lib/aws-s3-upload');
+
 let file = {
   path: process.argv[2],
   title: process.argv[3],
-};
-
-const s3Upload = () => {
-
-  let stream = fs.createReadStream(file.path);
-  let contentType = mime.lookup(file.path);
-  let ext = path.extname(file.path);
-  let folder = (new Date()).toISOString().split('T')[0];
-
-  const params = {
-    ACL: 'public-read',
-    Bucket: process.env.AWS_S3_BUCKET_NAME,
-    ContentType: contentType,
-    Key: 'keenannnnn wayans' + ext,
-    Body: stream,
-  };
-
-  return new Promise((resolve, reject) => {
-    s3.upload(params, (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-
-      console.log(err, data);
-    });
-  });
 };
 
 s3Upload(file)
