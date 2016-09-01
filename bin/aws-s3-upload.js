de5f610aa5611fe8107efa30bf93dbee759411d3
EchoXin <xin.yi@husky.neu.edu>
2016-09-01 13:39:37 -0400

    add upload model
    Models stores url and title of image uploaded to s3

--- bin/aws-s3-upload.js
+++ bin/aws-s3-upload.js
@@ -4,13 +4,20 @@ require('dotenv').load();
 
 const s3Upload = require('../lib/aws-s3-upload');
 
+const mongoose = require('../app/middleware/mongoose');
+const Upload = require('../app/models/upload');
+
 let file = {
   path: process.argv[2],
   title: process.argv[3],
 };
 
 s3Upload(file)
+  .then(s3response =>
+    Upload.create({
+      url: s3response.Location,
+      title: file.title,
+    }))
   .then(data => console.log(data))
-  .catch(err => console.error(err));
-
-console.log(file);
+  .catch(err => console.error(err))
+  .then(() => mongoose.connection.close());
