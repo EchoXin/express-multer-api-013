7058054c95ca618cd3dbf54b36a14c6b5d8d42f2
EchoXin <xin.yi@husky.neu.edu>
2016-09-01 10:40:12 -0400

    Add s3upload function
    wrap s3.upload in a promise

--- bin/aws-s3-upload.js
+++ bin/aws-s3-upload.js
@@ -5,23 +5,43 @@ require('dotenv').load();
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const fs = require('fs');
-
+const mime = require('mime');
+const path = require('path');
 let file = {
   path: process.argv[2],
   title: process.argv[3],
 };
 
-let stream = fs.createReadStream(file.path);
+const s3Upload = () => {
 
-const params = {
-  ACL: 'public-read',
-  Bucket: process.env.AWS_S3_BUCKET_NAME,
-  Key: 'keenan ivory wayans',
-  Body: stream,
+  let stream = fs.createReadStream(file.path);
+  let contentType = mime.lookup(file.path);
+  let ext = path.extname(file.path);
+  let folder = (new Date()).toISOString().split('T')[0];
+
+  const params = {
+    ACL: 'public-read',
+    Bucket: process.env.AWS_S3_BUCKET_NAME,
+    ContentType: contentType,
+    Key: 'keenannnnn wayans' + ext,
+    Body: stream,
+  };
+
+  return new Promise((resolve, reject) => {
+    s3.upload(params, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+
+      console.log(err, data);
+    });
+  });
 };
 
-s3.upload(params, function (err, data) {
-  console.log(err, data);
-});
+s3Upload(file)
+  .then(data => console.log(data))
+  .catch(err => console.error(err));
 
 console.log(file);
